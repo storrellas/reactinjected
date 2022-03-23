4e8ff399394dc636c941103ee6063d6e656e8c24
storrellas <storrellas@gmail.com>
2022-03-23 11:24:28 +0100

    ENH: Component react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 // function App() {
 //   return (
@@ -27,12 +28,25 @@ import './App.css';
 
 
 function App() {
+    const imgURL = 'https://www.creativefabrica.com/wp-content/uploads/2019/03/Coffee-cup-icon-by-LeisureProjects-580x387.jpg'
+    const [learnMore, setLearnMore] = useState(true);
     return (
       <div className="App" style={{ background: '#929FB8', height: '200px' }}>
         <div class="w-50 h-100 d-flex justify-content-center flex-column" style={{ margin: "0 auto"}}>
           <div>FREE ORIGIN MUGS & CHOCOLATES</div>
           <div>WITH COFFEE PURCHASE</div>
           <div>VERTUO: ORIGIN2022 | ORIGINAL: ORIGIN22</div>
+          <div class="mt-3 d-flex justify-content-center" style={{ height: '80px'}}>
+            {learnMore?
+            <div role="button" class="p-3 ms-3 btn-nespresso d-flex align-items-center" 
+              style={{background: "white", color:"black", maxWidth: "200px"}}
+              onClick={ () => setLearnMore(!learnMore)}>
+              Learn More
+            </div>
+            :
+            <img role="button" class="h-100" src={imgURL} onClick={ () => setLearnMore(!learnMore)}></img>
+            }
+          </div>
         </div>
       </div>
     );
