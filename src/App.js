cbe99b7d45727d64fb329dbefe81b3d3e437584c
storrellas <storrellas@gmail.com>
2022-03-23 09:39:43 +0100

    ENH: Creating simple Nespresso app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className="App" style={{ background: '#929FB8', height: '200px' }}>
+        <div class="w-50 h-100 d-flex justify-content-center flex-column" style={{ margin: "0 auto"}}>
+          <div>FREE ORIGIN MUGS & CHOCOLATES</div>
+          <div>WITH COFFEE PURCHASE</div>
+          <div>VERTUO: ORIGIN2022 | ORIGINAL: ORIGIN22</div>
+        </div>
+      </div>
+    );
+  }
 
 export default App;
